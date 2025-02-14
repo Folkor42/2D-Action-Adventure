b057581404631470fe101f429778cb870880faa2
James Nye <jnye@firstlight.net>
2025-02-14 10:56:12 -0500

    Episode Completed
    There is a Sprite Design issue with which hand holds the sword.

--- Items/Scripts/equipable_item_data.gd
+++ Items/Scripts/equipable_item_data.gd
@@ -3,3 +3,4 @@ class_name EquipableItemData extends ItemData
 enum Type { WEAPON, ARMOR, AMULET, RING }
 @export var type : Type = Type.WEAPON
 @export var modifiers : Array[ EquipableItemModifier ]
+@export var sprite_texture : Texture
