6aa51a90041acb3592736cbddb83976989a0ca58
James Nye <jnye@firstlight.net>
2025-03-06 11:05:28 -0500

    LevelTransitionInteract Added

---
+++ Levels/level_transition_interact.gd
@@ -0,0 +1,25 @@
+@tool
+class_name LevelTransitionInteract extends LevelTransition
+
+func _ready() -> void:
+	super()
+	area_entered.connect( _on_area_entered )
+	area_exited.connect( _on_area_exited )
+	pass
+	
+func player_interact () -> void:
+	print ("Player Interacted")
+	_player_entered( PlayerManager.player )
+	pass
+
+func _on_area_entered (_a : Area2D ) -> void:
+	PlayerManager.interact_pressed.connect( player_interact )
+	pass
+	
+func _on_area_exited (_a : Area2D ) -> void:
+	PlayerManager.interact_pressed.disconnect( player_interact )
+	pass
+	
+func _update_area() -> void:
+	super()
+	collision_shape.shape.size = Vector2( 32, 32 )
