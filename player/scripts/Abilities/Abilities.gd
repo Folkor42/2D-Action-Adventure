c3d73fbbb97dbea4ddc23861be1d9dddffb768fd
James Nye <jnye@firstlight.net>
2025-06-13 13:50:43 -0400

    Added Ability UI to Inventory and HUD

--- player/scripts/Abilities/Abilities.gd
+++ player/scripts/Abilities/Abilities.gd
@@ -2,20 +2,34 @@ class_name PlayerAbilites extends Node
 
 const BOOMERANG = preload("res://player/boomerang.tscn")
 
-enum abilities { BOOMERANG, GRAPPLE }
+var abilities : Array [ String ] = [ "BOOMERANG", "GRAPPLE", "BOW", "BOMB" ]
 
-var selected_ability = abilities.BOOMERANG
+var selected_ability : int = 0
 var player : Player
 var boomerang_instance : Boomerang = null
 
 func _ready() -> void:
 	player = PlayerManager.player
+	PlayerHud.update_arrow_count( player.arrow_count )
+	PlayerHud.update_bomb_count( player.bomb_count )
 
 func _unhandled_input( event : InputEvent ) -> void:
 	if event.is_action_pressed("ability"):
-		if selected_ability == abilities.BOOMERANG:
-			boomerang_ability()
+		match selected_ability:
+			0: boomerang_ability()
+			1: print("Grapple")
+			2: print("Bow")
+			3: print("Bomb")
+	
+	elif event.is_action_pressed("right_bumper") and !PauseMenu.is_paused:
+		toggle_ability(1)
+	elif event.is_action_pressed("left_bumper") and !PauseMenu.is_paused:
+		toggle_ability(-1)
 	pass
+
+func toggle_ability(change) -> void:
+	selected_ability=wrap(selected_ability + change,0,4)
+	PlayerHud.update_ability_ui(selected_ability)
 
 func boomerang_ability() -> void:
 	if boomerang_instance != null:
