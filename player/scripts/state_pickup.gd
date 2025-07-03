fa16ece09b2a2e2b30bd4d22020be3596c72ea1e
James Nye <folkor@gmail.com>
2025-07-03 19:12:44 -0400

    Added in Bomb Ability

--- player/scripts/state_pickup.gd
+++ player/scripts/state_pickup.gd
@@ -3,12 +3,16 @@ class_name State_Pickup extends State
 @export var pickup_audio : AudioStream
 @export var carry : State
 
+var start_anim_late : bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 	
 func Enter() -> void:
 	player.UpdateAnimation("pickup")
+	if start_anim_late:
+		player.animation_player.seek( 0.2 )
 	player.animation_player.animation_finished.connect( state_complete )
 	player.audio.stream = pickup_audio
 	player.audio.play()
@@ -20,7 +24,7 @@ func state_complete ( _a ) -> void:
 	pass
 
 func Exit() -> void:
-	print("Pickup complete")
+	start_anim_late = false
 	pass
 
 func Process( _delta : float ) -> State:
